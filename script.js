40b59f8832e4398734504a46300eca4fb547e138
peleg191 <pelegha2000@gmail.com>
2022-06-10 01:21:20 +0100

    progress.

--- script.js
+++ script.js
@@ -25,15 +25,21 @@ $(document).ready(function () {
     title.onclick = function (e) {
         if (!toggleClicked) {
             title.classList += 'animated wobble'
-            title.style.animationDuration='1.5s';
+            title.style.animationDuration = '1.5s';
             title.style.animationIterationCount = 1;
             toggleClicked = true;
             return;
         }
         title.classList = 'animated bounce';
         title.style.animationIterationCount = 'infinite';
-        title.style.animationDuration='3s';
+        title.style.animationDuration = '3s';
         toggleClicked = false;
-    }
+    };
 });
+
+function randomColor(){
+    let colorPool = ['#B06AB2','#E771A4','#FF858C','#FFA673','#FFCE65','#F9F871'];
+    let index = Math.floor(Math.random() * colorPool.length);
+    return colorPool[index];
+}
 
